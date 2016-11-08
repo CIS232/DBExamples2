aaede2f2323d988feb1efbea51b4ff4e53e36d04
SK207_inst <SK207_inst@K207_Inst.carrollcc.edu>
2016-11-07 21:01:08 -0500

    Examples from Class 11/7

---
+++ DBExamples2/src/main/java/edu/cis232/DBExamples2/App.java
@@ -0,0 +1,38 @@
+package edu.cis232.DBExamples2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws SQLException
+    {
+    	final String DB_URL = "jdbc:hsqldb:file:CoffeeDB/coffee";
+    	
+    	//Fields you'd like to extract from the data (Select columnNames)
+    	//Table(s) that you'd like to extract the data from (From tableName)
+    	//Optional Filter (where CONDITIONS)
+    	//Optional Ordering (order by columnNames desc)
+    	
+    	String sqlStatement = "SELECT * FROM COFFEE ORDER BY PRICE";
+    	
+    	Connection conn = DriverManager.getConnection(DB_URL);
+    	
+    	Statement stmt = conn.createStatement();
+    	
+    	ResultSet result = stmt.executeQuery(sqlStatement);
+    	
+    	while(result.next()){
+    		System.out.println(result.getString("description") + " " + result.getDouble("price"));
+    	}
+    	
+    	conn.close();
+    }
+}
